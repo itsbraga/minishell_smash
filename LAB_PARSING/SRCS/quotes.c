5e5109981a44e3e14f4d32ab140ad191317918e6
u4s2e0r <prioux.mateo@outlook.com>
2024-07-08 01:56:17 +0200

    quote almost done

--- LAB_PARSING/SRCS/quotes.c
+++ LAB_PARSING/SRCS/quotes.c
@@ -6,7 +6,7 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 20:42:22 by pmateo            #+#    #+#             */
-/*   Updated: 2024/07/06 17:08:48 by pmateo           ###   ########.fr       */
+/*   Updated: 2024/07/08 01:45:00 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,70 @@
 // Si single quote avec au moins un charactere a l'interieur = literal value except ENVVAR
 // Si double quote avec au moins un charactere a l'interieur = literal value
 
+// Cette fonction n'est pas suffisante
+// bool	unclosed_quotes(char *str)
+// {
+// 	int 	i;
+// 	int		nb_squote;
+// 	int		nb_dquote;
+	
+// 	i = 0;
+// 	nb_squote = 0;
+// 	nb_dquote = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '\'')
+// 			nb_squote++;
+// 		else if (str[i] == '"')
+// 			nb_dquote++;
+// 		i++;
+// 	}
+// 	if (nb_squote % 2 == 1 || nb_dquote % 2 == 1)
+// 		return (true);
+//	else
+// 		return (false);
+// }
+
+// Je parcours ma string
+// Je tombe sur un quote 
+//	Si je n'étais pas déjà dans des quotes alors closed = false pour que le quote soit marqué comme non fermé, mais si 
+// 	Si j'étais déjà dans des quotes alors closed = true et le quote est marqué comme fermé
+bool	unclosed_quotes_return(bool closed[])
+{
+	if (closed[0] == false || closed[1] == false)
+		return (true);
+	else
+		return (false);
+}
 
 bool	unclosed_quotes(char *str)
 {
-	int 	i;
-	int		nb_squote;
-	int		nb_dquote;
+	int	i;
+	bool	closed[2];
 	
 	i = 0;
-	nb_squote = 0;
-	nb_dquote = 0;
-	while (str[i])
+	closed[0] = true, closed[1] = true;
+	while(str[i])
 	{
 		if (str[i] == '\'')
-			nb_squote++;
+		{
+			if (closed[0] == true && closed[1] == true)
+				closed[0] = false;
+			else
+				closed[0] = true;
+		}
 		else if (str[i] == '"')
-			nb_dquote++;
+		{
+			if (closed[1] == true && closed[0] == true)
+				closed[1] = false;
+			else
+				closed[1] = true;
+		}
 		i++;
 	}
-	if (nb_squote % 2 == 1 || nb_squote % 2 == 1)
-		return (true);
-	return (false);
+	return (unclosed_quotes_return(closed));
 }
-char	*remove_quote(char *str, int quote_idx)
+char	*remove_empty_quotes(char *str, int quote_idx)
 {
 	int	i;
 	int	j;
@@ -64,7 +105,7 @@ char	*remove_quote(char *str, int quote_idx)
 }
 
 
-void	empty_quote(t_token *current)
+void	empty_quotes(t_token *current)
 {
 	char *str;
 	int		i;
@@ -75,7 +116,7 @@ void	empty_quote(t_token *current)
 	{
 		if ((str[i] == '"' && str[i + 1] == '"') || (str[i] == '\'' && str[i + 1] == '\''))
 		{
-			current->content = remove_quote(str, i);
+			current->content = remove_empty_quotes(str, i);
 			i = 0;
 			str = current->content;
 		}
@@ -99,7 +140,8 @@ void	handle_quotes(t_token **tok)
 			exit(EXIT_FAILURE);
 			// quote_exit();
 		}
-		empty_quote(current);
+		empty_quotes(current);
+		// others_quotes(current);
 		current = current->next;
 		size_list--;
 	}
