a26f107a788015489aa03bff6ab3c2cf09400726
Ana <it.braga@pm.me>
2024-08-18 19:40:18 +0200

    envp, join

--- SRCS/PARSING/envp.c
+++ SRCS/PARSING/envp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   envp.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: annabrag <annabrag@student.42.fr>          +#+  +:+       +#+        */
+/*   By: art3mis <art3mis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:41:26 by annabrag          #+#    #+#             */
-/*   Updated: 2024/08/13 15:48:38 by annabrag         ###   ########.fr       */
+/*   Updated: 2024/08/15 14:22:06 by art3mis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@
 
 char	*find_path(char **envp)
 {
-	int	i;
+	int		i;
+	char	*path_prefix;
 
 	i = 0;
-	while (envp[i] != '\0')
+	path_prefix = "PATH=";
+	while (envp[i] != NULL)
 	{
-		if (!strncmp(envp, "PATH=", 5))
+		if (ft_strncmp(envp[i], path_prefix, 5) == 0)
 			return (ft_substr(envp[i], 5, ft_strlen(envp[i] - 5)));
+		i++;
 	}
+	return (NULL);
 }
--- SRCS/PARSING/join.c
+++ SRCS/PARSING/join.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   join.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: annabrag <annabrag@student.42.fr>          +#+  +:+       +#+        */
+/*   By: art3mis <art3mis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:19:08 by annabrag          #+#    #+#             */
-/*   Updated: 2024/08/13 15:31:23 by annabrag         ###   ########.fr       */
+/*   Updated: 2024/08/18 19:40:00 by art3mis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// recoder strcat
 char	*cat_tcontent(t_token *to_cat)
 {
 	t_token	*tmp;
@@ -24,15 +25,15 @@ char	*cat_tcontent(t_token *to_cat)
 	while (tmp != NULL && tmp->content != NULL)
 	{
 		to_malloc += ft_strlen((char *)tmp->content);
-		tmp = tmp->content;	
+		tmp = tmp->next;	
 	}
 	buffer = ft_calloc((to_malloc + 1), sizeof(char));
 	if (!buffer)
 		return (NULL);
 	while (to_cat != NULL && to_cat->content != NULL)
 	{
-		ft_strcat(buffer, (char *)to_cat->content);
-		to_cat = to_cat->content;
+		strcat(buffer, (char *)to_cat->content);
+		to_cat = to_cat->next;
 	}
 	return (buffer);
 }
