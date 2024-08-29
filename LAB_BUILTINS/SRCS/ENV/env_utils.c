2282812ce734ff79a7a2d2b038a534741d59922e
Anna BRAGA <annabrag@made-f0Ar12s2.clusters.42paris.fr>
2024-08-29 23:04:23 +0200

    init of var_to_remove func for unset

--- LAB_BUILTINS/SRCS/ENV/env_utils.c
+++ LAB_BUILTINS/SRCS/ENV/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: annabrag <annabrag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:15:10 by pmateo            #+#    #+#             */
-/*   Updated: 2024/08/29 22:49:25 by annabrag         ###   ########.fr       */
+/*   Updated: 2024/08/29 23:03:55 by annabrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,19 @@ t_env	*env_new_var(char *content)
 	new_var->content = ft_strdup(content);
 	new_var->next = NULL;
 	return (new_var);
+}
+
+char	*var_to_remove(t_env *env, char *to_find)
+{
+	t_env	*head;
+	int		len_to_find;
+	
+	head = env;
+	len_to_find = ft_strlen(to_find);
+	while (head != NULL)
+	{
+		if (ft_strncmp(head->content, to_find, len_to_find) == 0);
+	}
 }
 
 void	del_env_var(t_env **env, t_env *var)
