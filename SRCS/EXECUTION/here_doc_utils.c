60232e0dc255b7f4f02d7afb941390c06f038ade
k0xdama <prioux.mateo@outlook.com>
2024-10-31 09:16:07 +0100

    Fix compilation error

--- SRCS/EXECUTION/here_doc_utils.c
+++ SRCS/EXECUTION/here_doc_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: annabrag <annabrag@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 02:26:44 by pmateo            #+#    #+#             */
-/*   Updated: 2024/10/31 08:33:49 by pmateo           ###   ########.fr       */
+/*   Updated: 2024/10/31 09:15:02 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static int	__close_heredoc(int fd[], char *limiter, char *line)
 		close(fd[0]);
 		return (STOP_EXEC);
 	}
-  
+}
 // void		__sigint_close_heredoc(int *fd, char *line, char *limiter)
 // {
 // 	dprintf(2, "PID : %d | closehd | cc\n", getpid());
